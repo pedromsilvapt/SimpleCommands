0fd63c2bcbead8809db615564861b252af8bddbc
Pedro Silva <pemiolsi@gmail.com>
2011-09-01 12:09:47 +0100

    Added the getHelp() function.

--- commands/invite.command.php
+++ commands/invite.command.php
@@ -1,1 +1,1 @@
-<?phpclass invite_command extends Command {		public function execute($userID, $username){		return('Função executada com sussesso.'.$userID.'\\'.$username);	}		}?>
+<?phpclass invite_command extends Command {		public function execute($userID, $username){		return('Função executada com sussesso.'.$userID.'\\'.$username);	}		public function getHelp(){		return('This is a test command. Call it whenever you want.');	}	}?>
